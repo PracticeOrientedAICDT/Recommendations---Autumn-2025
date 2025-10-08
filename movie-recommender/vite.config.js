4e7259b883771848621664f44fd2b8f14602dc2c
Guoda-Laur <guoda.laurinaviciute@bristol.ac.uk>
2025-10-08 11:59:32 +0100

    Add build site for GitHub Pages

--- movie-recommender/vite.config.js
+++ movie-recommender/vite.config.js
@@ -6,11 +6,20 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/movie-recommender/', 
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-recommender/', 
+  // 👇 VERY IMPORTANT: match your repo name exactly
+  base: '/Recommendations---Autumn-2025/',
 })
