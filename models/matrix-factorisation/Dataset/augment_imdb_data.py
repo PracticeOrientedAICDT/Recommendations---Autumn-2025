2de5b1c7b44e5df466ea7a0d2aeb5aba69127b8d
abbym-bristol <abby.morris@bristol.ac.uk>
2025-10-21 14:22:57 +0100

    feat: code to pull movie titles for imdb dataset working, have agreed to use a different dataset which has this information instead

--- models/matrix-factorisation/Dataset/augment_imdb_data.py
+++ models/matrix-factorisation/Dataset/augment_imdb_data.py
@@ -1,9 +1,19 @@
 # augment_imdb_data.py
 # Script to extract and store movie titles for imdb data
+import glob
+import os
+import re
 
 import numpy as np
 import imdb
 from imdb import IMDbError, IMDbDataAccessError
+
+# Functions
+def numericalSort(value):
+    numbers = re.compile(r'(\d+)')
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
 # Instance of cinemagoer class
 ia = imdb.Cinemagoer()
@@ -12,19 +22,36 @@ ia = imdb.Cinemagoer()
 dataset = np.load("Dataset/imdb-user-data/Dataset.npy")
 movie_ids = [data.split(',')[1][2:] for data in dataset]
 
-# Get movie titles and store
-movie_titles = []
+# Check server
 server_down = False
-for id in movie_ids:
-    try:
-        movie_titles.append(ia.get_movie(id))
-    except IMDbDataAccessError:
-        print("IMDB servers down - IMDbDataAccessError thrown, ending attempt to extract titles try again another time")
-        server_down = True
-        break
-    except IMDbError as err:
-        movie_titles.append(err)
+try:
+    ia.get_movie(movie_ids[0])
+except IMDbDataAccessError:
+    print("IMDB servers down - IMDbDataAccessError thrown, ending attempt to extract titles try again another time")
+    server_down = True
+
 
 if not server_down:
-    # Save this file
-    np.save("Dataset/imdb-user-data/movie_titles.npy", movie_titles)
+    # Make temp folder
+    os.makedirs("Dataset/imdb-user-data/temp", exist_ok=True)
+    # Get movie titles and store
+    movie_titles = []
+    count = 0
+    for id in movie_ids:
+        try:
+            movie_titles.append(ia.get_movie(id)["title"])
+        except IMDbError as err:
+            movie_titles.append(err)
+        count += 1
+
+        if count % 50 == 0:
+            np.save(f"Dataset/imdb-user-data/temp/movie_titles_{count}.npy", movie_titles)
+            movie_titles = []
+            print(f"{count} titles retrieved")
+
+    # Load and combine title datasets
+    complete_movie_titles = []
+    for infile in sorted(glob.glob('Dataset/imdb-user-data/temp/movie_titles_*.npy'), key=numericalSort):
+        print("Processing file: " + infile)
+        complete_movie_titles.extend(np.load(infile))
+    np.save(f"Dataset/imdb-user-data/movie_titles.npy", complete_movie_titles)
